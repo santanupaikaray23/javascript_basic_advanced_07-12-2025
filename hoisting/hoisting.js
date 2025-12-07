c304af7ff85b8a2fda984cc3fc37dabff9f5bdc9
santanupaikaray23 <santanupaikaray1996@gmail.com>
2025-12-07 19:26:31 +0530

    adding some changes

---
+++ hoisting/hoisting.js
@@ -0,0 +1,22 @@
+b();
+console.log(a);
+var a = "hello world"
+function b(){
+    console.log('called b')
+}
+// This will not any exceptions this will give output OfflineAudioCompletionEvent
+// b method and returns "undefines for a"
+console.log(a)
+console.log(b)
+c()
+d()
+var a = 10;
+var b = 5;
+function c(){
+    console.log('called c')
+}
+function d(){
+    console.log('called d')
+}
+console.log(a)
+console.log(b)
---
+++ undefined/undefined.js
@@ -0,0 +1,33 @@
+var a;
+console.log(a); 
+
+if(a === undefined){
+    console.log("a is undefined");
+}
+//==checks for value(a=10)
+//===check for value and type
+
+var a = "10";
+console.log(a==10)
+console.log(a===10)
+// ==if a is 10 or not checking if a is a number or String
+// ===if a is 10 and a is a integer
+
+var a =10; // number
+    a="santanu"//string
+    a=true//boolean
+    a=null//null
+    a=undefined//undefined
+    a={}//object
+    a=[]//array
+    a=function(){}//function
+    console.log(a)
+
+    var a = 1;
+    console.log(a)
+
+    if(a === undefined){
+        console.log("a is undefined");
+    }else{
+        console.log("a is defined");
+    }
