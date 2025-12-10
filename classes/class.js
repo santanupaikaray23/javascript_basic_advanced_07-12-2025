a79e80a3c5864ece5c652661587ec58f8d7d73a1
santanupaikaray23 <santanupaikaray1996@gmail.com>
2025-12-10 21:56:33 +0530

    adding some changes

---
+++ classes/class.js
@@ -0,0 +1,33 @@
+// Class
+
+
+ function Student(){
+    
+ }
+
+// instance
+
+
+function Person(){
+
+}
+
+var person1 = new Person();
+var person2 = new Person();
+
+// constructor
+
+ function Order(){
+    consle.log('Order is placed')
+ }
+
+ var order = new Order(); 
+
+//  Properties
+
+ function customer(gender){
+    this.gender = gender
+ }
+
+ var customer1 = new Customer('Male')
+ var customer2 = new Customer('Male')
---
+++ classes/prototype.js
@@ -0,0 +1,14 @@
+function Student(){
+    this.name = "Santanu"
+    this.gender = "Female"
+    this.age = 23;
+
+}
+
+var stuObj1 = new Student();
+stuObj1.age = 24; 
+
+console.log(stuObj1.age);
+
+var stuObj1 = new Student();
+console.log(stuObj2.age)
